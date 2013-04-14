188caea57dd456fc90d965d06bc740a048f16cb1
Shriphani Palakodety <shriphanip@gmail.com>
2013-04-14 16:13:35 -0400

    added code to clean and stem words in the data

---
+++ cleaner.py
@@ -0,0 +1,48 @@
+"""
+Cleaner exports a set of functions to do stemming etc.
+"""
+
+#!/usr/bin/env python
+
+import string
+
+from nltk.stem import porter
+
+STEMMER = porter.PorterStemmer()
+LINE_OPS = [
+	lambda s : ' '.join(s.split()), # newlines tabs etc taken care of
+	lambda s : ' '.join([w for w in s.split() if not w.startswith('http')]), #nuke links
+	lambda s : s.translate(
+		string.maketrans(
+			string.punctuation + string.uppercase + string.digits,
+			' '*len(string.punctuation) + string.lowercase + ' '*len(string.digits)
+		)
+	),  # nukes out punctuation, digits and converts to lowercase
+	lambda s : ''.join(
+		map(
+			lambda c : c if c in string.printable else ' ',
+			s
+		)
+	),  # ensures ASCII only
+	lambda s : ' '.join(
+		map(
+			lambda w : STEMMER.stem(w),
+			s.split()
+		)
+	),
+	lambda s : ' '.join(
+		filter(
+			lambda w : len(w) > 3,
+			s.split()
+		)
+	)	# ensures that only string of length 3 and above are in the user text
+]
+
+
+def clean_text(text):
+	global LINE_OPS
+
+	for op in LINE_OPS:
+		text = op(text)
+
+	return ' '.join(text.split())
